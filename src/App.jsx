c24013f93b4a2d2360101d4ab4bca2c388973e5b
Mariia Voitenko <mariiavoitenko@Mac.lan>
2024-10-22 12:23:42 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header>
+      <h3>School of English</h3>
+
+      <span>Тут будет время</span>
+    </header>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Hello React! </h1>
+      </main>
+    </div>
+  );
+}
