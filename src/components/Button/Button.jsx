2fca0a150c454ef6fccb704a17cf744ff856ffea
Mariia Voitenko <mariiavoitenko@Mac.lan>
2024-11-01 12:45:59 +0000

    Working with forms

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,10 @@
-import './Button.css'
+import classes from './Button.module.css'
 
-export default function Button({ children, onClick, isActive }) {
-    // let classes = 'button'
-    // if (isActive) classes += ' active'
-
+export default function Button({ children, isActive, ...props }) {
     return (
-        <button className={isActive ? 'button active' : 'button'} onClick={onClick}>{children}
+        <button
+            {...props}
+            className={isActive ? `${classes.button} ${classes.active}` : classes.button}>{children}
         </button>
     )
 }
--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,7 +1,54 @@
+import { useState } from 'react'
+import Button from './Button/Button'
+
 export default function FeedbackSection() {
+    const [name, setName] = useState('')
+    const [hasError, setHasError] = useState(true)
+    const [reason, setReason] = useState('help')
+
+    function handleNameChange(event) {
+        console.log(event.target.value)
+        setName(event.target.value)
+        setHasError(event.target.value.trim().length === 0)
+    }
+
+    function toggleError() {
+        setHasError((prev) => !prev)
+    }
+
     return (
         <section>
             <h3>Feedback</h3>
+
+            <Button onClick={toggleError}>Toggle Error</Button>
+
+            <form>
+                <label htmlFor="name">Your name</label>
+                <input
+                    type="text"
+                    id="name"
+                    className="control"
+                    value={name}
+                    style={{
+                        border: hasError ? '1px solid red' : null
+                    }}
+                    onChange={handleNameChange} />
+
+                <label htmlFor="reason">Reason for contacting</label>
+                <select id="reason" className="control" value={reason} onChange={event => setReason(event.target.value)}>
+                    <option value="error">Error</option>
+                    <option value="help">Need help</option>
+                    <option value="suggest">Proposal</option>
+                </select>
+
+                <pre>
+                    Name: {name}
+                    <br />
+                    Reason: {reason}
+                </pre>
+
+                <Button disabled={hasError} isActive={!hasError}>Send</Button>
+            </form>
         </section>
     )
 }
